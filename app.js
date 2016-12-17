7480bc819f24ac21b18f8c7e93d78030283186e0
BAnderson <estaban555@gmail.com>
2016-12-17 02:00:28 +0000

    Added documentation, and changed it so it at least runs on cloud9

--- app.js
+++ app.js
@@ -4,31 +4,29 @@ var bodyParser = require('body-parser');
 var pgp = require('pg-promise')();
 var db = pgp('postgres://postgres:1234@localhost:5432/highscores');
 
-// this is to serve the css and js from the public folder to your app
-// it's a little magical, but essentially you put files in there and link  
-// to them in you head of your files with css/styles.css
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var currentScore;
-var numEntries;
-var lowScore;
-const MAXHIGHSCORES = 10;
+var currentScore; // the current score recieved by the player
+var numEntries; // the number of high scores in the database
+var lowScore; // the lowest high score in the database
+const MAXHIGHSCORES = 10; // the number of high scores allowed into the database
 
+// sets the numEntries and the lowScore variables, based on the database, then renders the quiz screen
 app.get('/', function(req,res,next){
     currentScore = 0;
-    db.one('SELECT COUNT (*) FROM highscores')
+    db.one('SELECT COUNT (*) FROM highscores') // gets the current number of entries from the database
         .then(function (result){
             numEntries = parseInt(result.count);
         })
         .catch(function(err){
             console.log(err);
         })
-    db.one('SELECT MIN(score) FROM highscores' )
-        .then(function(result){
+    db.one('SELECT MIN(score) FROM highscores' ) // gets the smallest score entry in the database, and sets that as the low score
+        .then(function(result){ // used to determine if a score is a high score or not
             lowScore = parseInt(result.min);
         })
         .catch(function(err){
@@ -38,7 +36,8 @@ app.get('/', function(req,res,next){
     res.render('index');
 });
 
-
+// calculates the player's score. If it's a high score, then it directs the player to enter their name
+// otherwise, it just goes straight to the high score screen
 app.get('/submit', function(req,res,next){
     currentScore = 0;
     if(req.body.QuestionOne != '') currentScore++;
@@ -50,14 +49,14 @@ app.get('/submit', function(req,res,next){
     //console.log(numEntries);
     //console.log(MAXHIGHSCORES);
     
-    if(numEntries < MAXHIGHSCORES){
+    if(numEntries < MAXHIGHSCORES){ // if the high scores database isn't full, the player is guaranteed a high score
         numEntries++;
         res.render('enterScore', {score:currentScore});
     }
-    else if(currentScore > lowScore){
+    else if(currentScore > lowScore){ // if the high score database is full, and the player's score is higher
         db.result('delete from blogs where id = $1', MAXHIGHSCORES) // deletes the last entry in the database
             .then(function (result) {
-                 res.render('enterScore', {score:currentScore});
+                 res.render('enterScore', {score:currentScore}); // renders the "enter score" page
             })
             .catch(function (err) {
               return next(err);
@@ -68,6 +67,7 @@ app.get('/submit', function(req,res,next){
     }
 });
 
+// enters the score into the database, then goes to the high score screen
 app.post('/logScore', function(req, res, next){
     db.none('insert into highscores(player, score)' + 'values($1, $2)', [req.body.player, currentScore])
         .then(function () {
@@ -78,6 +78,7 @@ app.post('/logScore', function(req, res, next){
         });
 });
 
+// renders the high score screen, sorted with the highest score at the top
 app.get('/hiscores', function(req, res, next){
     db.any('SELECT * FROM highscores ORDER BY score DESC')
         .then(function(data){
@@ -88,6 +89,7 @@ app.get('/hiscores', function(req, res, next){
         });
 });
 
+// starts the server
 app.listen(process.env.PORT, function(){
-    console.log('Application running on localhost on port ' + process.env.PORT);
+    console.log('Application running at https://milestone-2-banderson.c9users.io/');
 });
--- highscores.sql
+++ highscores.sql
@@ -1,19 +1,10 @@
 DROP DATABASE IF EXISTS highscores;
 CREATE DATABASE highscores;
-
 \c highscores;
-
-CREATE TABLE highscores (
-  ID SERIAL PRIMARY KEY,
-  player VARCHAR,
-  score INTEGER
-);
-
+CREATE TABLE highscores (ID SERIAL PRIMARY KEY,player VARCHAR,score INTEGER);
 INSERT INTO highscores (player, score)
   VALUES ('Tyler', 4);
-
 INSERT INTO highscores (player, score)
   VALUES ('Bill', 4);
-
 INSERT INTO highscores (player, score)
   VALUES ('Phillip', 0);
